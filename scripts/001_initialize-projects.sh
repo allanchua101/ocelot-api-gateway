ed3550f8b156ad3af70b81de1a60ec4ee643c3a0
Allan Chua <allanchua.officefiles@gmail.com>
2018-08-26 18:06:01 +0800

    -Created shell scripts for generating API Gateway Aggregation Demo

---
+++ scripts/001_initialize-projects.sh
@@ -0,0 +1,7 @@
+dotnet new webapi --name ServiceA
+
+dotnet new webapi --name ServiceB
+
+dotnet new webapi --name ServiceC
+
+dotnet new webapi --name Gateway
---
+++ scripts/002_initialize-solution.sh
@@ -0,0 +1,6 @@
+dotnet new sln --name GatewayDemo
+
+dotnet sln GatewayDemo.sln add ServiceA/ServiceA.csproj
+dotnet sln GatewayDemo.sln add ServiceB/ServiceB.csproj
+dotnet sln GatewayDemo.sln add ServiceC/ServiceC.csproj
+dotnet sln GatewayDemo.sln add Gateway/Gateway.csproj
