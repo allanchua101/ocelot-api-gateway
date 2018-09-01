80f618e79ce974f9f6d936b004371dea72f1df47
Allan Chua <allanchua.officefiles@gmail.com>
2018-09-01 09:51:35 +0800

    -Sample API gateway code

---
+++ final/Gateway/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Ocelot.Middleware;
+using Ocelot.DependencyInjection;
+
+namespace Gateway
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
+                          .UseStartup<Startup>()
+                          .ConfigureAppConfiguration((hostingContext, config) =>
+                          {
+                              config.AddJsonFile("ocelot.json")
+                              .AddEnvironmentVariables();
+                          })
+                          .ConfigureServices(s =>
+                          {
+                              s.AddOcelot();
+                          })
+                          .ConfigureLogging((hostingContext, logging) =>
+                          {
+                              //add your logging
+                          })
+                          .UseIISIntegration()
+                          .Configure(app =>
+                          {
+                              app.UseOcelot().Wait();
+                          })
+                          .Build();
+        }
+    }
+}
